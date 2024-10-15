63a369a531bba1dbb0c87e171f356e4894ed2294
Giougt <alexandre18golf@gmail.com>
2024-10-16 00:08:14 +0200

    re_code function

--- script .js
+++ script .js
@@ -1,3 +1,7 @@
+const input_result = document.createElement("input"); 
+document.body.appendChild(input_result);
+
+// css part 
 Object.assign(document.body.style,{
     height: "100vh",
     display: "flex",
@@ -5,26 +9,51 @@ Object.assign(document.body.style,{
     alignItems: "center"
 })
 
-const star_style = document.getElementsByClassName("lar la-star");
-for (let i = 0; i < star_style.length; i++) {
-    Object.assign(star_style[i].style, {
-        color: "#f5e609",
-        fontSize: "100px"
-    });
-}
+//star 1 
+const star10 = document.getElementById("stars1"); 
+Object.assign(star10.style, {
+    color: "#f5e609",
+    fontSize: "100px"
+});
 
+//star 2 
+const star20 = document.getElementById("stars2"); 
+Object.assign(star20.style, {
+    color: "#f5e609",
+    fontSize: "100px"
+});
 
-const input_result = document.createElement("input"); 
-document.body.appendChild(input_result);
+//star 3
+const star30 = document.getElementById("stars3"); 
+Object.assign(star30.style, {
+    color: "#f5e609",
+    fontSize: "100px"
+});
+
+//star 4
+const star40 = document.getElementById("stars4"); 
+Object.assign(star40.style, {
+    color: "#f5e609",
+    fontSize: "100px"
+});
+
+//star 5
+const star50 = document.getElementById("stars5"); 
+Object.assign(star50.style, {
+    color: "#f5e609",
+    fontSize: "100px"
+});
 
 Object.assign(input_result.style,{
     marginLeft: "50px"
 })
 
+//end css 
 
 function value(){
-    stars10.addEventListener("click", function(){
-        let a = document.getElementById("stars10").getAttribute("data-value");
-        input_result.value= a; 
+    star10.addEventListener("click", function(){
+        star10.style.color = "red"; 
     });
 }
+
+value();
