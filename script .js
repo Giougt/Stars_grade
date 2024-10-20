f19568fc4cb9215c1104a1f05ff71db9ad9e6981
Giougt <alexandre18golf@gmail.com>
2024-10-20 23:20:20 +0200

    first sucess for all star

--- script .js
+++ script .js
@@ -12,35 +12,35 @@ Object.assign(document.body.style,{
 //star 1 
 const star10 = document.getElementById("stars1"); 
 Object.assign(star10.style, {
-    color: "#f5e609",
+    color: "#777771",
     fontSize: "100px"
 });
 
 //star 2 
 const star20 = document.getElementById("stars2"); 
 Object.assign(star20.style, {
-    color: "#f5e609",
+    color: "#777771",
     fontSize: "100px"
 });
 
 //star 3
 const star30 = document.getElementById("stars3"); 
 Object.assign(star30.style, {
-    color: "#f5e609",
+    color: "#777771",
     fontSize: "100px"
 });
 
 //star 4
 const star40 = document.getElementById("stars4"); 
 Object.assign(star40.style, {
-    color: "#f5e609",
+    color: "#777771",
     fontSize: "100px"
 });
 
 //star 5
 const star50 = document.getElementById("stars5"); 
 Object.assign(star50.style, {
-    color: "#f5e609",
+    color: "#777771",
     fontSize: "100px"
 });
 
@@ -50,10 +50,49 @@ Object.assign(input_result.style,{
 
 //end css 
 
-function value(){
-    star10.addEventListener("click", function(){
-        star10.style.color = "red"; 
-    });
-}
+// part script 
 
-value();
+const ele = [star10,star20,star30,star40,star50];
+
+// void reset 
+
+function reset_star() {
+    for (let index = 0; index < ele.length; index++) {
+        ele[index].style.color = "#777771";
+    }
+};
+
+// void each star 
+    star10.addEventListener("click", function(){
+        reset_star(); 
+        star10.style.color = "#e530ea";
+        
+    });
+
+    star20.addEventListener("click", function(){
+        reset_star(); 
+        for (let index = 0; index < ele.length-3; index++) {
+            ele[index].style.color = "#e530ea";
+        }
+    });
+
+    star30.addEventListener("click", function(){
+        reset_star(); 
+        for (let index = 0; index < ele.length-2; index++) {
+            ele[index].style.color = "#e530ea";
+        }
+    });
+
+    star40.addEventListener("click", function(){
+        reset_star(); 
+        for (let index = 0; index < ele.length-1; index++) {
+            ele[index].style.color = "#e530ea";
+        }
+    });
+
+    star50.addEventListener("click", function(){
+        reset_star(); 
+        for (let index = 0; index < ele.length; index++) {
+            ele[index].style.color = "#e530ea";
+        }
+    });
