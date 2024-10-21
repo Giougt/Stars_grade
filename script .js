49fe81f8aed62cb61cac01245490d47c36eb02e6
Giougt <alexandre18golf@gmail.com>
2024-10-21 23:54:49 +0200

    delete input for result and re-size stars

--- script .js
+++ script .js
@@ -1,6 +1,3 @@
-const input_result = document.createElement("input"); 
-document.body.appendChild(input_result);
-
 // css part 
 Object.assign(document.body.style,{
     height: "100vh",
@@ -13,40 +10,37 @@ Object.assign(document.body.style,{
 const star10 = document.getElementById("stars1"); 
 Object.assign(star10.style, {
     color: "#777771",
-    fontSize: "100px"
+    fontSize: "120px"
 });
 
 //star 2 
 const star20 = document.getElementById("stars2"); 
 Object.assign(star20.style, {
     color: "#777771",
-    fontSize: "100px"
+    fontSize: "120px"
 });
 
 //star 3
 const star30 = document.getElementById("stars3"); 
 Object.assign(star30.style, {
     color: "#777771",
-    fontSize: "100px"
+    fontSize: "120px"
 });
 
 //star 4
 const star40 = document.getElementById("stars4"); 
 Object.assign(star40.style, {
     color: "#777771",
-    fontSize: "100px"
+    fontSize: "120px"
 });
 
 //star 5
 const star50 = document.getElementById("stars5"); 
 Object.assign(star50.style, {
     color: "#777771",
-    fontSize: "100px"
+    fontSize: "120px"
 });
 
-Object.assign(input_result.style,{
-    marginLeft: "50px"
-})
 
 //end css 
 
